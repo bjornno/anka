b20b949af93cac0b5ae3579186159adbbb1e1dee
Bjørn Nordlund <bnordlun@cisco.com>
2016-09-11 14:32:03 +0200

    syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :anka, Anka.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "fast-stream-59170.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
