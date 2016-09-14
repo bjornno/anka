84fccad54398feee0b4ceee9eb961d8c9bf23fd1
Bjørn Nordlund <bnordlun@cisco.com>
2016-09-14 23:22:24 +0200

    stuff

--- web/controllers/weather_controller.ex
+++ web/controllers/weather_controller.ex
@@ -11,23 +11,22 @@ defmodule Anka.WeatherController do
   def create(conn, _param) do
     %{"actorId" => _, "created" => _, "data" => %{"created" => _, "id" => message_id, "personEmail" => sender_email, "personId" => _, "roomId" => _, "roomType" => _}, "event" => "created", "id" => _, "name" => "anka", "resource" => "messages", "targetUrl" => "https://fast-stream-59170.herokuapp.com/api/weather"} = _param
 
-    url = "https://api.ciscospark.com/v1/messages/#{message_id}"
+    unless "kalle@sparkbot.io" = sender_email do
+      query = Anka.Spark.evaluate(message_id)
+      luis_result = Anka.Luis.evaluate(query)
+      case luis_result do
+        ["weather", place] -> 
+          Anka.WeatherData.update_temperature(Anka.WeatherData, place)
+          temp = Anka.WeatherData.get_temp(Anka.WeatherData)
+          Anka.Endpoint.broadcast "weather:temp", "new_temp", %{temp: temp}
+        false ->
+          Anka.Endpoint.broadcast "weather:temp", "new_temp", %{temp: "did not understand the question"}
+      end
 
-    {:ok, {{_, 200, _}, _, body}} = :httpc.request(:get, {String.to_char_list(url), [{'Authorization', 'Bearer NmYxNTBlZTYtZjU4ZS00MTU2LTlkNWUtNzc3MjQ3MjQ5MjJlNjUyYTJhNDQtMWI1'}]}, [], [])
-    
-    decoded = Poison.decode!(body)
-
-    %{"created" => _, "id" => _, "personEmail" => person_email, "personId" => _, "roomId" => _, "roomType" => _, "text" => query} = decoded
-
-    luis_result = Anka.Luis.evaluate(query)
-    case luis_result do
-      ["weather", place] -> 
-        Anka.WeatherData.update_temperature(Anka.WeatherData, place)
-        temp = Anka.WeatherData.get_temp(Anka.WeatherData)
-        Anka.Endpoint.broadcast "weather:temp", "new_temp", %{temp: temp}
-      false ->
-        Anka.Endpoint.broadcast "weather:temp", "new_temp", %{temp: "did not understand the question"}
     end
     render conn, []
   end
+
+  
+
 end
